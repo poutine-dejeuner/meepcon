b8ce68c38e844966a3526c3eb252a48905f8f630
letournv <letournv@mila.quebec>
2025-01-30 15:34:37 -0500

    opt_size_x, opt_size_y sont ecrases dans le script python lumerical.py le script from_lsf.py a ete modifie en fonction

--- Tutoral_3/from_lsf.py
+++ Tutoral_3/from_lsf.py
@@ -11,15 +11,19 @@ import pint
 from utils import double_with_mirror, normalise
 
 
-def mapping(x,eta,beta):
+def mapping(x, eta, beta):
     # eta in [0,1], skews the distribution towards one material or the other?
     # higher beta makes design more binarized
-    x = (npa.fliplr(x.reshape(Nx,Ny)) + x.reshape(Nx,Ny))/2 # up-down symmetry
+    # up-down symmetry
+    ic(x.shape)
+    ic(Nx, Ny)
+    x = (npa.fliplr(x.reshape(Nx, Ny)) + x.reshape(Nx, Ny))/2
     # filter
-    filtered_field = mpa.conic_filter(x,
-        filter_radius,design_region_width,design_region_height,design_region_resolution)
+    filtered_field = mpa.conic_filter(x, filter_radius, design_region_width,
+                                      design_region_height,
+                                      design_region_resolution)
     # projection
-    projected_field = mpa.tanh_projection(filtered_field,beta,eta)
+    projected_field = mpa.tanh_projection(filtered_field, beta, eta)
     # interpolate to actual materials
     return projected_field.flatten()
 
@@ -44,17 +48,25 @@ PATH = os.path.expanduser(PATH)
 # wg_index = 2.8;
 # bg_index = 1.44;
 # dx = 20e-9;
-opt_size_x=3.5e-6*ur.meter
-opt_size_y=3.5e-6*ur.meter
-size_x=opt_size_x+0.6e-6*ur.meter
-size_y=opt_size_y+1e-6*ur.meter
+
+# size_x et size_y sont ecrases par le script python
+# opt_size_x = 3.5e-6*ur.meter
+# opt_size_y = 3.5e-6*ur.meter
+dx = 20e-9*ur.meter/ur.pixel
+opt_size_x = 2000e-9*ur.meter + dx*ur.pixel
+opt_size_y = 2*1800e-9*ur.meter + dx*ur.pixel
+size_x = opt_size_x+0.6e-6*ur.meter
+size_y = opt_size_y+1e-6*ur.meter
 out_wg_dist = 1.25e-6*ur.meter
 wg_width = 0.5e-6*ur.meter
 mode_width = 3*wg_width
 wg_index = 2.8
 bg_index = 1.44
-dx = 20e-9*ur.meter/ur.pixel
 wg_zspan = 220e-9*ur.meter
+
+opt_xpixel = opt_size_x*(1/dx)
+opt_ypixel = opt_size_y*(1/dx)
+ic(opt_xpixel.magnitude, opt_ypixel.magnitude)
 
 # ## GEOMETRY
 # #INPUT WAVEGUIDE
@@ -169,30 +181,42 @@ center_wavelength = 1550e-9*ur.meter
 # set('y span',mode_width);
 
 
-
-Si = mp.Medium(index=wg_index) # Effective permittivity for a Silicon waveguide with a thickness of 220nm
+# Effective permittivity for a Silicon waveguide with a thickness of 220nm
+Si = mp.Medium(index=wg_index)
 SiO2 = mp.Medium(index=bg_index)
-delta = dx.to(ur.micrometers/ur.pixel) #(0.02)*ur.micrometers  size of a pixel (in μm) 20 nm in lumerical exp
+# (0.02)*ur.micrometers  size of a pixel (in μm) 20 nm in lumerical exp
+delta = dx.to(ur.micrometers/ur.pixel)
 # resolution = 20 # (pixels/μm)
-resolution = 1/delta # pixels/μm
-waveguide_width = (wg_width).to(ur.micrometer) #0.5 # (μm)
+resolution = 1/delta  # pixels/μm
+waveguide_width = (wg_width).to(ur.micrometer)  # 0.5 # (μm)
 
-#The above settings are not compatible with the (101, 91) design shapes of the previous experiment. the values below replace the optimisation regions with compatible shapes
-half_design_shape = (101,91)
-design_sizex = 101*ur.pixel*dx
-design_sizey = 91*ur.pixel*dx
-# design_region_width = opt_size_x.to(ur.micrometer) # (μm)
-# design_region_height = opt_size_y.to(ur.micrometer) # (μm)
-design_region_width = design_sizex.to(ur.micrometer) # (μm)
-design_region_height = 2*design_sizey.to(ur.micrometer) # (μm)
+# The above settings are not compatible with the (101, 91) design shapes of the
+# previous experiment. the values below replace the optimisation regions with
+# compatible shapes
+# design_shape= (101, 182)
+# design_sizex = design_shape[0]*ur.pixel*dx
+# design_sizey = design_shape[1]*ur.pixel*dx
+design_region_width = opt_size_x.to(ur.micrometer)  # (μm)
+design_region_height = opt_size_y.to(ur.micrometer)  # (μm)
+# design_region_width = design_sizex.to(ur.micrometer)  # (μm)
+# design_region_height = design_sizey.to(ur.micrometer)  # (μm)
 
-arm_separation = (out_wg_dist).to(ur.micrometer) #1.0  (μm) distance between arms center to center
+# 1.0  (μm) distance between arms center to center
+arm_separation = (out_wg_dist).to(ur.micrometer)
 
-ic(design_region_height*resolution)
-waveguide_length = (source_wg_xmax - source_wg_xmin).to(ur.micrometer) #0.5  (μm)
-pml_size = 1.0 # (μm)
+# ic((design_region_height*resolution).magnitude)
+# ic(design_region_width*resolution)
+# test = (int((design_region_width*resolution).magnitude),
+#         int((design_region_height*resolution).magnitude))
+# ic(test)
+# assert test == design_shape
 
-ic(resolution, waveguide_length, waveguide_width, design_region_height, design_region_width, arm_separation)
+waveguide_length = (
+    source_wg_xmax - source_wg_xmin).to(ur.micrometer)  # 0.5  (μm)
+pml_size = 1.0  # (μm)
+
+ic(resolution, waveguide_length, waveguide_width,
+   design_region_height, design_region_width, arm_separation)
 
 delta = delta.magnitude
 resolution = resolution.magnitude
@@ -206,50 +230,59 @@ source_yspan = source_yspan.magnitude
 
 # ## Design variable setup
 
-minimum_length = 0.09 # (μm)
+minimum_length = 0.09  # (μm)
 eta_e = 0.75
-filter_radius = mpa.get_conic_radius_from_eta_e(minimum_length,eta_e) # (μm)
+filter_radius = mpa.get_conic_radius_from_eta_e(minimum_length, eta_e)  # (μm)
 eta_i = 0.5
 eta_d = 1-eta_e
-design_region_resolution = int(resolution) #int(4*resolution) # (pixels/μm)
-frequencies = 1/np.linspace(1.5,1.6,5) # (1/μm)
+design_region_resolution = int(resolution)  # int(4*resolution) # (pixels/μm)
+frequencies = 1/np.linspace(1.5, 1.6, 5)  # (1/μm)
 
-Nx = int(design_region_resolution*design_region_width)
-Ny = int(design_region_resolution*design_region_height)
+Nx = round(design_region_resolution*design_region_width)
+Ny = round(design_region_resolution*design_region_height)
 
-design_variables = mp.MaterialGrid(mp.Vector3(Nx,Ny),SiO2,Si)
+size = mp.Vector3(design_region_width, design_region_height)
+design_variables = mp.MaterialGrid(mp.Vector3(Nx, Ny), SiO2, Si)
 design_region = mpa.DesignRegion(design_variables,
-    volume=mp.Volume(center=mp.Vector3(), size=mp.Vector3(design_region_width, design_region_height)))
+                                 volume=mp.Volume(center=mp.Vector3(),
+                                                  size=size))
 
 
 # ## Simulation Setup
 
-Sx = 2*pml_size + 2*waveguide_length + design_region_width # cell size in X
-Sy = 2*pml_size + design_region_height + 0.5 # cell size in Y
-cell_size = mp.Vector3(Sx,Sy)
+Sx = 2*pml_size + waveguide_length + design_region_width  # cell size in X
+Sy = 2*pml_size + design_region_height + 0.5  # cell size in Y
+cell_size = mp.Vector3(Sx, Sy)
 
 pml_layers = [mp.PML(pml_size)]
 
-fcen = (1/center_wavelength.to(ur.micrometer)).magnitude #1/1.55
+fcen = (1/center_wavelength.to(ur.micrometer)).magnitude  # 1/1.55
 width = 0.2
 fwidth = width * fcen
-source_center  = [-Sx/2 + pml_size + waveguide_length/3,0,0]
-source_size = mp.Vector3(0,source_yspan, 0) #mp.Vector3(0,2,0)
-kpoint = mp.Vector3(1,0,0)
-src = mp.GaussianSource(frequency=fcen,fwidth=fwidth)
+source_center = [-Sx/2 + pml_size + waveguide_length/3, 0, 0]
+source_size = mp.Vector3(0, source_yspan, 0)  # mp.Vector3(0,2,0)
+kpoint = mp.Vector3(1, 0, 0)
+src = mp.GaussianSource(frequency=fcen, fwidth=fwidth)
 source = [mp.EigenModeSource(src,
-                            eig_band=1,
-                            direction=mp.NO_DIRECTION,
-                            eig_kpoint=kpoint,
-                            size=source_size,
-                            center=source_center,
-                            eig_parity=mp.EVEN_Z+mp.ODD_Y)]
+                             eig_band=1,
+                             direction=mp.NO_DIRECTION,
+                             eig_kpoint=kpoint,
+                             size=source_size,
+                             center=source_center,
+                             eig_parity=mp.EVEN_Z+mp.ODD_Y)]
 
 geometry = [
-    mp.Block(center=mp.Vector3(x=-Sx/4), material=Si, size=mp.Vector3(Sx/2+1, waveguide_width, 0)), # left waveguide
-    mp.Block(center=mp.Vector3(x=Sx/4, y=arm_separation/2), material=Si, size=mp.Vector3(Sx/2+1, waveguide_width, 0)), # top right waveguide
-    mp.Block(center=mp.Vector3(x=Sx/4, y=-arm_separation/2), material=Si, size=mp.Vector3(Sx/2+1, waveguide_width, 0)), # bottom right waveguide
-    mp.Block(center=design_region.center, size=design_region.size, material=design_variables)
+    # left waveguide
+    mp.Block(center=mp.Vector3(x=-Sx/4), material=Si,
+             size=mp.Vector3(Sx/2+1, waveguide_width, 0)),
+    # top right waveguide
+    mp.Block(center=mp.Vector3(x=Sx/4, y=arm_separation/2), material=Si,
+             size=mp.Vector3(Sx/2+1, waveguide_width, 0)),
+    # bottom right waveguide
+    mp.Block(center=mp.Vector3(x=Sx/4, y=-arm_separation/2), material=Si,
+             size=mp.Vector3(Sx/2+1, waveguide_width, 0)),
+    mp.Block(center=design_region.center,
+             size=design_region.size, material=design_variables)
 ]
 
 # index map from Lumerical settings
@@ -271,8 +304,9 @@ sim = mp.Simulation(cell_size=cell_size,
                     default_material=SiO2,
                     resolution=resolution)
 
-sim.plot2D()
-plt.show()
+# sim.plot2D()
+# plt.show()
+
 
 def get_sim_coeffs_from_flux_region(sim, fluxregion):
     sim.reset_meep()
@@ -295,7 +329,8 @@ source_flux_spectrum = get_sim_coeffs_from_flux_region(sim, fluxregion)
 # plt.show()
 
 # Get top output flux coefficients
-topmoncenter = mp.Vector3(Sx/2 - pml_size - 2*waveguide_length/3, arm_separation/2, 0)
+topmoncenter = mp.Vector3(
+    Sx/2 - pml_size - 2*waveguide_length/3, arm_separation/2, 0)
 topfluxregion = mp.FluxRegion(topmoncenter, monsize)
 top_flux_spectrum = get_sim_coeffs_from_flux_region(sim, topfluxregion)
 # sim.plot2D()
@@ -305,11 +340,12 @@ FOM2 = (top_flux_spectrum[0])/source_flux_spectrum[0]
 ic(FOM2)
 ic(np.load(os.path.join(PATH, 'fom.npy'), mmap_mode='r')[image_idx])
 
-_, axes = plt.subplots(1,3)
+_, axes = plt.subplots(1, 3)
 sim.plot2D(fields=mp.Ex, ax=axes[0])
 axes[0].set_title('Ex')
 sim.plot2D(fields=mp.Ey, ax=axes[1])
 axes[1].set_title('Ey')
 sim.plot2D(fields=mp.Hz, ax=axes[2])
 axes[2].set_title('Hz')
-plt.show()
+plt.save('meepfields.npy')
+# plt.show()
